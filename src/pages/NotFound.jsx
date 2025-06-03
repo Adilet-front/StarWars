40115001da950048f18083194572ebb758473af5
Adilet333 <adilettinadilettin@gmail.com>
2025-06-03 19:24:23 +0600

    +NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,23 @@
+import { NavLink } from "react-router";
+import "../styles/NotFound.scss";
+
 export const NotFound = () => {
-  return(
-    <div>
-      <h1>NotFound</h1>
+  return (
+    <div className="NotFound">
+      <div className="NotFDText">
+        <h1>
+          4
+          <img
+            src="https://www.svgrepo.com/show/440402/death-star.svg"
+            alt=""
+          />
+          4
+        </h1>
+        <NavLink to={"/"}>Кликни чтобы перейти на главную страницу</NavLink>
+      </div>
+      <div className="DarkLordImg">
+   
+      </div>
     </div>
-  )
-}
+  );
+};
