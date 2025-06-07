bb62c710974cff6058de62a1989fe761bb67ab08
Adilet333 <adilettinadilettin@gmail.com>
2025-06-07 13:26:31 +0600

    Fail-page

--- src/pages/FailPage.jsx
+++ src/pages/FailPage.jsx
@@ -1,7 +1,22 @@
-export const FailPage= () => {
-  return(
-    <div>
-      <h1>FailPage</h1>
-    </div>
-  )
-}
+import { NavLink } from "react-router";
+import "../styles/FailPage.scss";
+
+export const FailPage = () => {
+  return (
+    <section className="Failpage">
+      <div className="Failimage">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/1633/1633157.png"
+          alt=""
+        />
+      </div>
+      <div className="TextFailpages">
+        <h2>Error</h2>
+        <p>sorry, page not found</p>
+        <button>
+          <NavLink to={"/"}>Back to home</NavLink>
+        </button>
+      </div>
+    </section>
+  );
+};
